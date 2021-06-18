4551a73e9ec0b30b2fc8a22fff9484882da6d0b2
matchi01 <mateusz.w.twardy@gmail.com>
2021-06-18 23:33:31 +0200

    Added component StyledInputAuth

---
+++ src/components/atoms/StyledInputAuth.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+const StyledInputAuth = styled.input`
+    margin-bottom: 30px;
+    padding: 2px 0;
+    padding-bottom: 5px;
+    width: 250px;
+    background-color: transparent;
+    border: none;
+    outline: none;
+    border-bottom: 1.5px solid;
+    border-bottom-color: grey;
+    font-weight: 700;
+    color: ${({theme}) => theme.colors.blue};
+    opacity: 0.55;
+`;
+
+export default StyledInputAuth;
