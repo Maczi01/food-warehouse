b21848b3b4142f0ba889856a1c649e21147bbab7
matchi01 <mateusz.w.twardy@gmail.com>
2021-06-20 23:20:29 +0200

    Change login method (remove useCallback)

--- src/views/LoginView.js
+++ src/views/LoginView.js
@@ -2,26 +2,44 @@ import React, {useCallback, useContext, useState} from 'react';
 import styled from 'styled-components';
 import LoginForm from "../components/organisms/LoginForm";
 import {AuthContext} from "../providers/Auth";
-import {auth} from '../firebase/firebaseConfig'
+import {auth, db} from '../firebase/firebaseConfig'
 import {Redirect} from 'react-router-dom';
 import AuthTemplate from "../components/templates/AuthTemplate";
 
 const LoginView = ({history}) => {
         const [previousLoginAttemptFailed, setPreviousLoginAttemptFailed] = useState(false)
-        const handleLogin = useCallback(
-            async event => {
-                event.preventDefault();
-                const {email, password} = event.target.elements;
-                try {
-                    await auth.signInWithEmailAndPassword(email.value, password.value);
+        // const handleLogin = useCallback(
+        //     async event => {
+        //         event.preventDefault();
+        //         const {email, password} = event.target.elements;
+        //         try {
+        //             await auth.signInWithEmailAndPassword(email.value, password.value);
+        //             setPreviousLoginAttemptFailed(false);
+        //             history.push("/");
+        //         } catch (err) {
+        //             console.error("Login Error");
+        //             setPreviousLoginAttemptFailed(true);
+        //         }
+        //     }, [history]
+        // );
+
+    const handleLogin = event => {
+        event.preventDefault();
+        const {email, password} = event.target.elements;
+        auth.signInWithEmailAndPassword(email.value, password.value)
                     setPreviousLoginAttemptFailed(false);
-                    history.push("/");
-                } catch (err) {
-                    console.error("Login Error");
-                    setPreviousLoginAttemptFailed(true);
-                }
-            }, [history]
-        );
+
+        // .then(cred => {
+            //     return db.collection('users').doc(cred.user.uid)
+            //         .set({
+            //             userMail: email.value
+            //         });
+            // });
+        // history.push("/")
+    }
+
+
+
         const {currentUser} = useContext(AuthContext);
         if (currentUser) {
             return <Redirect to="/"/>
