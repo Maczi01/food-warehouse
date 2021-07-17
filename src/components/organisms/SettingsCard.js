c86962a6aa4ed79f50cb4cc6fde1d07084246e82
matchi01 <mateusz.w.twardy@gmail.com>
2021-07-17 23:35:18 +0200

    Installed userEvent library

--- src/components/organisms/SettingsCard.js
+++ src/components/organisms/SettingsCard.js
@@ -1,170 +1,172 @@
-import React, {useContext} from 'react';
+import React, { useContext } from "react";
 import styled from "styled-components";
-import user from '../../assets/img/user.svg'
-import {auth} from "../../firebase/firebaseConfig";
-import {FormattedMessage} from "react-intl";
-import {AppContext} from "../../context/context";
-
+import user from "../../assets/img/user.svg";
+import { auth } from "../../firebase/firebaseConfig";
+import { FormattedMessage } from "react-intl";
+import { AppContext } from "../../context/context";
 
 const SettingsWrapper = styled.div`
-      display: flex;
-      justify-content: center;
-      flex-direction: column;
-      margin: 0 auto;
-      padding: 20px;
-      width: 50vw;
-      @media (max-width: ${({theme}) => theme.mobile}) {
-        width: 100vw;
-     }
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  margin: 0 auto;
+  padding: 20px;
+  width: 50vw;
+  @media (max-width: ${({ theme }) => theme.mobile}) {
+    width: 100vw;
+  }
 `;
 
 const UserCard = styled.div`
-      display: flex;
-      justify-content: space-between;
-      width: 40vw;
-      margin: 0 auto;
-      padding: 15px 0;
-`
+  display: flex;
+  justify-content: space-between;
+  width: 40vw;
+  margin: 0 auto;
+  padding: 15px 0;
+`;
 
 const UserAvatarWrapper = styled.img`
-      width: 100px;
-      height: 100px;
-      display: flex;
-      justify-content: center;
-      margin: 0 auto;
-`
+  width: 100px;
+  height: 100px;
+  display: flex;
+  justify-content: center;
+  margin: 0 auto;
+`;
 const UserMailWrapper = styled.div`
-     display: flex;
-     align-items: center;
-     flex-direction: column;
-`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+`;
 const Button = styled.button`
-    width: 170px;
-    height: 50px;
-    outline: none;
-    border: none;
-    font-size: 20px;
-    font-weight: 600;
-    letter-spacing: 5px;
-    border-radius: 5px;
-    cursor: pointer;
-    background: ${({theme}) => theme.colors.pink};
-    color: ${({theme}) => theme.colors.white};
-`
+  width: 170px;
+  height: 50px;
+  outline: none;
+  border: none;
+  font-size: 20px;
+  font-weight: 600;
+  letter-spacing: 5px;
+  border-radius: 5px;
+  cursor: pointer;
+  background: ${({ theme }) => theme.colors.pink};
+  color: ${({ theme }) => theme.colors.white};
+`;
 
 const OptionsWrapper = styled.div`
-      width: 100%;
-      display: flex;
-      flex-direction: column;
-      margin: 0 auto;
-`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  margin: 0 auto;
+`;
 const OptionsItem = styled.div`
       width: 100%;
       display: flex;
       justify-content: center;
-               @media (max-width: ${({theme}) => theme.mobile}) {
+               @media (max-width: ${({ theme }) => theme.mobile}) {
           flex-direction: column;
-          border: 1px solid   ${({theme}) => theme.colors.darkblue});
+          border: 1px solid   ${({ theme }) => theme.colors.darkblue});
   
     }
-`
+`;
 const StyledLabel = styled.label`
-      background-color: ${({theme}) => theme.colors.blue};
-      color: ${({theme}) => theme.colors.white};
-      text-align: center;
-      justify-content: center;  
-      transition: 0.2s ease-out all;
-      font-size: 18px;
-      height: 70px;
-      width: 300px;
-      margin: 6px;
-      display: flex;
-      text-decoration: none;
-      align-items: center;
-      @media (max-width: ${({theme}) => theme.mobile}) {
-          height: 50px;
-      }
-`
+  background-color: ${({ theme }) => theme.colors.blue};
+  color: ${({ theme }) => theme.colors.white};
+  text-align: center;
+  justify-content: center;
+  transition: 0.2s ease-out all;
+  font-size: 18px;
+  height: 70px;
+  width: 300px;
+  margin: 6px;
+  display: flex;
+  text-decoration: none;
+  align-items: center;
+  @media (max-width: ${({ theme }) => theme.mobile}) {
+    height: 50px;
+  }
+`;
 
 const StyledSelect = styled.select`
-    display: block;
-    appearance: none;
-    outline: 0;
-    border: 1px solid white;
-    width: 300px;
-    border-radius: 3px;
-    margin: 6px;
-    text-align: center;
-    font-size: 18px;
-    color: black;
-    transition-duration: 0.25s;
-    font-weight: 300;
-    background-color: ${({theme}) => theme.colors.gray};
-      text-align-last:center;
-               @media (max-width: ${({theme}) => theme.mobile}) {
-          height: 50px;
-    }
-`
+  display: block;
+  appearance: none;
+  outline: 0;
+  border: 1px solid white;
+  width: 300px;
+  border-radius: 3px;
+  margin: 6px;
+  text-align: center;
+  font-size: 18px;
+  color: black;
+  transition-duration: 0.25s;
+  font-weight: 300;
+  background-color: ${({ theme }) => theme.colors.gray};
+  text-align-last: center;
+  @media (max-width: ${({ theme }) => theme.mobile}) {
+    height: 50px;
+  }
+`;
 const Heading = styled.h1`
-     padding: 10px;
-     color: ${({theme}) => theme.colors.blue};
-     text-align: center;
-     display: flex;
-     justify-content: center;
-     align-items: center;
-     flex-direction: column;
-     margin: 0 auto;
-     width: 75vw;
-`
+  padding: 10px;
+  color: ${({ theme }) => theme.colors.blue};
+  text-align: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  margin: 0 auto;
+  width: 75vw;
+`;
 const Paragraph = styled.p`
-    padding: 15px;
-`
-
+  padding: 15px;
+`;
 
 const SettingsCard = () => {
-        const {handleLanguageChange, toggleTheme, language} = useContext(AppContext);
+  const { handleLanguageChange, toggleTheme, language } =
+    useContext(AppContext);
 
-        return (
-            <>
-                <Heading>
-                    <FormattedMessage id="settings"/>
-                </Heading>
-                <SettingsWrapper>
-                    <UserCard>
-                        <UserAvatarWrapper src={user} alt="Avatar icon"/>
-                        <UserMailWrapper>
-                            <Paragraph>
-                                <FormattedMessage id="logged as"/>
-                                {auth.currentUser.email}
-                            </Paragraph>
-                            <Button onClick={() => auth.signOut()}>
-                                <FormattedMessage id="logout"/>
-                            </Button>
-                        </UserMailWrapper>
-                    </UserCard>
-                    <OptionsWrapper>
-                        <OptionsItem>
-                            <StyledLabel>
-                                <FormattedMessage id="change language"/>
-                            </StyledLabel>
-                            <StyledSelect onChange={handleLanguageChange} defaultValue={language.locale}>
-                                <option value="pl">Polski</option>
-                                <option value="en">English</option>
-                            </StyledSelect>
-                        </OptionsItem>
-                        <OptionsItem>
-                            <StyledLabel>
-                                <FormattedMessage id="dark mode"/>
-                            </StyledLabel>
-                            <StyledSelect onChange={toggleTheme} defaultValue="off">
-                                <option value="on" label="on"/>
-                                <option value="off" label="off"/>
-                            </StyledSelect>
-                        </OptionsItem>
-                    </OptionsWrapper>
-                </SettingsWrapper>
-            </>)
-    }
-;
-
+  return (
+    <>
+      <Heading>
+        <FormattedMessage id="settings" />
+      </Heading>
+      <SettingsWrapper>
+        <UserCard>
+          <UserAvatarWrapper src={user} alt="Avatar icon" />
+          <UserMailWrapper>
+            <Paragraph>
+              <FormattedMessage id="logged as" />
+              {auth.currentUser.email}
+            </Paragraph>
+            <Button onClick={() => auth.signOut()}>
+              <FormattedMessage id="logout" />
+            </Button>
+          </UserMailWrapper>
+        </UserCard>
+        <OptionsWrapper>
+          <OptionsItem>
+            <StyledLabel>
+              <FormattedMessage id="change language" />
+            </StyledLabel>
+            <StyledSelect
+              onChange={handleLanguageChange}
+              defaultValue={language.locale}
+              data-testid="select-option"
+            >
+              <option value="pl">Polski</option>
+              <option value="en">English</option>
+            </StyledSelect>
+          </OptionsItem>
+          <OptionsItem>
+            <StyledLabel>
+              <FormattedMessage id="dark mode" />
+            </StyledLabel>
+            <StyledSelect onChange={toggleTheme} defaultValue="off">
+              <option value="on" label="on" />
+              <option value="off" label="off" />
+            </StyledSelect>
+          </OptionsItem>
+        </OptionsWrapper>
+      </SettingsWrapper>
+    </>
+  );
+};
 export default SettingsCard;
