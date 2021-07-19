22b7482f90353652e181d74a3341e18b9d4b9daa
matchi01 <mateusz.w.twardy@gmail.com>
2021-07-19 22:55:08 +0200

    Added more tests for Item form

--- src/__tests__/SettingsView.test.js
+++ src/__tests__/SettingsView.test.js
@@ -1,19 +1,18 @@
-import {render, screen, waitFor} from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import React from "react";
-import {ThemeProvider} from "styled-components";
-import {lightTheme} from "../theme/theme";
-import {IntlProvider} from "react-intl";
-import {EN_language as language} from "../utills/language";
-import {BrowserRouter} from "react-router-dom";
+import { ThemeProvider } from "styled-components";
+import { lightTheme } from "../theme/theme";
+import { IntlProvider } from "react-intl";
+import { EN_language as language } from "../utills/language";
+import { BrowserRouter } from "react-router-dom";
 import AppProvider from "../context/context";
-import {AuthProvider} from "../providers/Auth";
+import { AuthProvider } from "../providers/Auth";
 import user from "@testing-library/user-event";
 import ItemForm from "../components/organisms/ItemForm";
 
-describe("Test Add product view", () => {
+describe("Test Add product form tests, ", () => {
   it("correctly call submit function", async () => {
     const onSubmitMock = jest.fn();
-
     render(
       <AuthProvider>
         <AppProvider>
@@ -55,6 +54,81 @@ describe("Test Add product view", () => {
         minimalQuantity: 5,
         maximalQuantity: 5,
       })
+    );
+  });
+
+  it("correctly show error message when field is not filled and do not submit form", async () => {
+    const onSubmitMock = jest.fn();
+    render(
+      <AuthProvider>
+        <AppProvider>
+          <ThemeProvider theme={lightTheme}>
+            <IntlProvider locale={language.locale} messages={language.lang}>
+              <BrowserRouter>
+                <ItemForm addItem={onSubmitMock} />
+              </BrowserRouter>
+            </IntlProvider>
+          </ThemeProvider>
+        </AppProvider>
+      </AuthProvider>
+    );
+
+    const inputName = screen.getByTestId("name");
+    const inputCategory = screen.getByTestId("category");
+    const inputMaximalQuantity = screen.getByTestId("maximalQuantity");
+    const inputMinimalQuantity = screen.getByTestId("maximalQuantity");
+    const inputCurrentQuantity = screen.getByTestId("currentQuantity");
+
+    const submitButton = screen.getByTestId("accept");
+
+    user.type(inputName, "Wine");
+    user.selectOptions(inputCategory, ["dairy"]);
+    user.type(inputMaximalQuantity, "5");
+    user.type(inputMinimalQuantity, "5");
+    user.type(inputCurrentQuantity, "5");
+    user.click(submitButton);
+
+    await waitFor(() => expect(onSubmitMock).toHaveBeenCalledTimes(0));
+    await waitFor(() =>
+      expect(screen.getByText("Unit is required!")).toBeInTheDocument()
+    );
+  });
+
+  it("correctly show error message when number is too bit", async () => {
+    const onSubmitMock = jest.fn();
+    render(
+      <AuthProvider>
+        <AppProvider>
+          <ThemeProvider theme={lightTheme}>
+            <IntlProvider locale={language.locale} messages={language.lang}>
+              <BrowserRouter>
+                <ItemForm addItem={onSubmitMock} />
+              </BrowserRouter>
+            </IntlProvider>
+          </ThemeProvider>
+        </AppProvider>
+      </AuthProvider>
+    );
+
+    const inputName = screen.getByTestId("name");
+    const inputCategory = screen.getByTestId("category");
+    const inputUnit = screen.getByTestId("unit");
+    const inputMaximalQuantity = screen.getByTestId("maximalQuantity");
+    const inputMinimalQuantity = screen.getByTestId("minimalQuantity");
+    const inputCurrentQuantity = screen.getByTestId("currentQuantity");
+    const submitButton = screen.getByTestId("accept");
+
+    user.type(inputName, "Wine");
+    user.selectOptions(inputCategory, ["dairy"]);
+    user.selectOptions(inputUnit, ["liter"]);
+    user.type(inputMaximalQuantity, "55");
+    user.type(inputMinimalQuantity, "5");
+    user.type(inputCurrentQuantity, "5");
+    user.click(submitButton);
+
+    await waitFor(() => expect(onSubmitMock).toHaveBeenCalledTimes(0));
+    await waitFor(() =>
+      expect(screen.getByText("Less than 10!")).toBeInTheDocument()
     );
   });
 });
