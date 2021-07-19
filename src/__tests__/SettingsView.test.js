84e13b84a0c7cc65a5d3c6a801b045566261ffe1
matchi01 <mateusz.w.twardy@gmail.com>
2021-07-19 22:11:31 +0200

    Added correct test for form.

--- src/__tests__/SettingsView.test.js
+++ src/__tests__/SettingsView.test.js
@@ -1,33 +1,32 @@
-import { act, render, screen } from "@testing-library/react";
+import {render, screen, waitFor} from "@testing-library/react";
 import React from "react";
-import { ThemeProvider } from "styled-components";
-import { lightTheme } from "../theme/theme";
-import { IntlProvider } from "react-intl";
-import { EN_language as language } from "../utills/language";
-import { BrowserRouter } from "react-router-dom";
+import {ThemeProvider} from "styled-components";
+import {lightTheme} from "../theme/theme";
+import {IntlProvider} from "react-intl";
+import {EN_language as language} from "../utills/language";
+import {BrowserRouter} from "react-router-dom";
 import AppProvider from "../context/context";
-import { AuthProvider } from "../providers/Auth";
-import AddView from "../views/AddView";
-import userEvent from "@testing-library/user-event";
+import {AuthProvider} from "../providers/Auth";
 import user from "@testing-library/user-event";
-import { waitFor } from "@testing-library/react";
+import ItemForm from "../components/organisms/ItemForm";
 
 describe("Test Add product view", () => {
-  it("correctly displays button for each category", async () => {
+  it("correctly call submit function", async () => {
+    const onSubmitMock = jest.fn();
+
     render(
       <AuthProvider>
         <AppProvider>
           <ThemeProvider theme={lightTheme}>
             <IntlProvider locale={language.locale} messages={language.lang}>
               <BrowserRouter>
-                <AddView />
+                <ItemForm addItem={onSubmitMock} />
               </BrowserRouter>
             </IntlProvider>
           </ThemeProvider>
         </AppProvider>
       </AuthProvider>
     );
-    const onSubmitMock = jest.fn();
 
     const inputName = screen.getByTestId("name");
     const inputCategory = screen.getByTestId("category");
@@ -37,21 +36,25 @@ describe("Test Add product view", () => {
     const inputCurrentQuantity = screen.getByTestId("currentQuantity");
     const submitButton = screen.getByTestId("accept");
 
-    act(() => {
-      userEvent.type(inputName, "30");
-      userEvent.selectOptions(inputCategory, ["dairy"]);
-      userEvent.selectOptions(inputUnit, ["liter"]);
-      userEvent.type(inputMaximalQuantity, "5");
-      userEvent.type(inputMinimalQuantity, "5");
-      userEvent.type(inputCurrentQuantity, "5");
-      user.click(submitButton);
-    });
+    user.type(inputName, "Wine");
+    user.selectOptions(inputCategory, ["dairy"]);
+    user.selectOptions(inputUnit, ["liter"]);
+    user.type(inputMaximalQuantity, "5");
+    user.type(inputMinimalQuantity, "5");
+    user.type(inputCurrentQuantity, "5");
+    user.click(submitButton);
+
+    await waitFor(() => expect(onSubmitMock).toBeCalled());
+    await waitFor(() => expect(onSubmitMock).toHaveBeenCalledTimes(1));
+    await waitFor(() =>
+      expect(onSubmitMock).toHaveBeenCalledWith({
+        name: "Wine",
+        category: "dairy",
+        unit: "liter",
+        currentQuantity: 5,
+        minimalQuantity: 5,
+        maximalQuantity: 5,
+      })
+    );
   });
 });
-// const subtractButton = screen.getByTestId("generatePdf");
-
-// expect(subtractButton).toBeInTheDocument();
-
-// userEvent.selectOptions(getByTestId('select-option'), 'Polski');
-// const label = screen.getByText("tryb ciemny");
-//
