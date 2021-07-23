22881d8ee50b619032dc117fc450fc8ebd04660e
matchi01 <mateusz.w.twardy@gmail.com>
2021-07-23 23:48:08 +0200

    Added test for polish language

--- src/__tests__/Navbar.test.js
+++ src/__tests__/Navbar.test.js
@@ -1,11 +1,12 @@
-import {render, screen} from "@testing-library/react";
-import {lightTheme} from "../theme/theme";
+import { render, screen } from "@testing-library/react";
+import { lightTheme } from "../theme/theme";
 import Navbar from "../components/molecules/Navbar";
-import {ThemeProvider} from "styled-components";
-import {BrowserRouter} from "react-router-dom";
+import { ThemeProvider } from "styled-components";
+import { BrowserRouter } from "react-router-dom";
 import AppProvider from "../context/context";
-import {IntlProvider} from "react-intl";
-import {EN_language as language} from "../utills/language";
+import { IntlProvider } from "react-intl";
+import { EN_language } from "../utills/language";
+import { PL_language  } from "../utills/language";
 import React from "react";
 
 describe("Navbar tests, ", () => {
@@ -13,7 +14,7 @@ describe("Navbar tests, ", () => {
     render(
       <AppProvider>
         <ThemeProvider theme={lightTheme}>
-          <IntlProvider locale={language.locale} messages={language.lang}>
+          <IntlProvider locale={EN_language.locale} messages={EN_language.lang}>
             <BrowserRouter>
               <Navbar />
             </BrowserRouter>
@@ -22,16 +23,42 @@ describe("Navbar tests, ", () => {
       </AppProvider>
     );
 
-    const checkInvnetoryButton = screen.getByTestId("checkInventory");
+    const checkInventoryButton = screen.getByTestId("checkInventory");
     const addProductButton = screen.getByTestId("addProduct");
     const shoppingListButton = screen.getByTestId("shoppingList");
     const settingsButton = screen.getByTestId("settings");
     const backgroundColor = screen.getByTestId("navbarBackground");
 
-    expect(checkInvnetoryButton).toBeInTheDocument();
+    expect(checkInventoryButton).toBeInTheDocument();
     expect(addProductButton).toBeInTheDocument();
     expect(shoppingListButton).toBeInTheDocument();
     expect(settingsButton).toBeInTheDocument();
     expect(backgroundColor).toHaveStyle("background: #00214D");
   });
+
+    it("correctly render Navbar with navigation buttons in polish", () => {
+        render(
+            <AppProvider>
+                <ThemeProvider theme={lightTheme}>
+                    <IntlProvider locale={PL_language.locale} messages={PL_language.lang}>
+                        <BrowserRouter>
+                            <Navbar />
+                        </BrowserRouter>
+                    </IntlProvider>
+                </ThemeProvider>
+            </AppProvider>
+        );
+
+        const checkInventoryButton = screen.getByTestId("checkInventory");
+        const addProductButton = screen.getByTestId("addProduct");
+        const shoppingListButton = screen.getByTestId("shoppingList");
+        const settingsButton = screen.getByTestId("settings");
+        const backgroundColor = screen.getByTestId("navbarBackground");
+
+        expect(checkInventoryButton).toBeInTheDocument();
+        expect(addProductButton).toBeInTheDocument();
+        expect(shoppingListButton).toBeInTheDocument();
+        expect(settingsButton).toBeInTheDocument();
+        expect(backgroundColor).toHaveStyle("background: #00214D");
+    });
 });
