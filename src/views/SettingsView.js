d15ffc3c3249cbd02231fec3645c49546d96297e
matchi01 <mateusz.w.twardy@gmail.com>
2021-07-31 23:26:23 +0200

    Changes in structure (moved auth components to views)

--- src/views/SettingsView.js
+++ src/views/SettingsView.js
@@ -1,11 +1,17 @@
 import React from "react";
 import SettingsCard from "../components/organisms/SettingsCard";
 import MainTemplate from "../components/templates/MainTemplate";
+import {auth} from "../firebase/firebaseConfig";
 
-const SettingsView = () => (
-  <MainTemplate>
-    <SettingsCard />
-  </MainTemplate>
-);
+const SettingsView = () => {
+  const currentMail = auth.currentUser.email;
+    const signOut = () => auth.signOut();
+
+  return (
+    <MainTemplate>
+      <SettingsCard signOut={signOut} currentMail={currentMail}/>
+    </MainTemplate>
+  );
+};
 
 export default SettingsView;
