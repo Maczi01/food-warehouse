7c43e24c8802e88c490ade2ee72b28936609de6d
mludwig <mlu@spyro-soft.com>
2023-02-05 10:46:35 +0100

    fix: fix product edit page

--- src/app/features/Product/containers/edit-view.component.js
+++ src/app/features/Product/containers/edit-view.component.js
@@ -1,19 +1,14 @@
 import { useParams } from 'react-router-dom';
 
-import MainTemplate from '../../../layouts/default-layout/default-layout.component';
 import { useInventory } from '../../../services/inventory.store';
 import EditItemFormComponents from '../components/edit-item-form.component';
 
 const EditViewComponent = () => {
   const { id: selectedId } = useParams();
-  const { foodList, editItem } = useInventory();
-  const item = foodList.find((item) => item.id === selectedId);
+  const { state, editItem } = useInventory();
+  const item = state.inventory.find((item) => item.id === selectedId);
 
-  return (
-    <MainTemplate>
-      <EditItemFormComponents item={item} editItem={editItem} />
-    </MainTemplate>
-  );
+  return <EditItemFormComponents item={item} editItem={editItem} />;
 };
 
 export default EditViewComponent;
