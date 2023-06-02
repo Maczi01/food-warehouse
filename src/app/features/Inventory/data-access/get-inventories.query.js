62c5f34e723e3d07ea97c0fd75197d42ce9b8dfd
Maczi01 <mateusz.w.twardy@gmail.com>
2023-06-02 07:55:04 +0100

    feat: implemented zod

--- src/app/features/Inventory/data-access/get-inventories.query.js
+++ src/app/features/Inventory/data-access/get-inventories.query.js
@@ -1,11 +1,18 @@
-import { useQuery } from 'react-query';
+import {useQuery} from 'react-query';
+import {z} from 'zod';
 
-import { getAllInventories } from './infrastructure/get-all-invetories';
-import { queryKey } from './inventory.query-key';
+import {getAllInventories} from './infrastructure/get-all-invetories';
+import {queryKey} from './inventory.query-key';
 
 export const useGetInventoriesQuery = () => {
-  return useQuery({
-    queryKey: queryKey.inventory(),
-    queryFn: getAllInventories,
-  });
+    return useQuery({
+        queryKey: queryKey.inventory(),
+        queryFn: getAllInventories,
+        onError: (error) => {
+            if (error instanceof z.ZodError) {
+                // eslint-disable-next-line no-console
+                console.log('error from useGetInventoriesQuery', error)
+            }
+        },
+    });
 };
